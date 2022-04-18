42fb50f2ebf971fabfa0edabde8c711542ae1b16
Dinotator <56325558+Dinotator@users.noreply.github.com>
2022-04-17 22:56:57 -0500

    Add files via upload

---
+++ store/cart/mutations.js
@@ -0,0 +1,84 @@
+export default {
+    LoadCart(state) {
+        let cart = localStorage.getItem("freeCart");
+        if (cart) {
+            state.cart = JSON.parse(cart);
+        }
+    },
+    AddToCart(state, product) {
+        //Check if item is in cart
+        let itemfound = state.cart.find((p) => p.product.id === product.id);
+        if (!itemfound) {
+            state.cart.push({ product, quantity: 1 });
+        }
+        if (itemfound) {
+            itemfound.quantity += 1;
+        }
+
+        //Update local storage
+        localStorage.setItem("freeCart", JSON.stringify(state.cart));
+
+        this.$swal({
+            toast: true,
+            text: "Cart Updated.",
+            icon: "success",
+            timer: 2000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            position: "top-end",
+        });
+    },
+    DecreaseItemCount(state, index) {
+        let item = state.cart[index];
+        if (!item) return;
+        if (item.quantity == 1) {
+            state.cart.splice(index, 1);
+        } else {
+            item.quantity -= 1;
+        }
+        this.$swal({
+            toast: true,
+            text: "Cart Updated.",
+            icon: "success",
+            timer: 2000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            position: "top-end",
+        });
+    },
+    RemoveCartItem(state, index) {
+        state.cart.splice(index, 1);
+
+        this.$swal({
+            toast: true,
+            text: "Item Removed.",
+            icon: "success",
+            timer: 2000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            position: "top-end",
+        });
+        localStorage.setItem("freeCart", JSON.stringify(state.cart));
+    },
+    IncreaseItemCount(state, index) {
+        let item = state.cart[index];
+        item.quantity += 1;
+        this.$swal({
+            toast: true,
+            text: "Cart Updated.",
+            icon: "success",
+            timer: 2000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            position: "top-end",
+        });
+        localStorage.setItem("freeCart", JSON.stringify(state.cart));
+
+    },
+    ClearCart(state) {
+        state.cart = [];
+        //Update local storage
+        localStorage.removeItem("freeCart");
+    },
+};
+
